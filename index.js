ca81f8b697d6c2f03dff3c28054222c52f2d4c80
Alejandra Rodriguez <malejart@gmail.com>
2025-07-23 23:29:42 +0200

    Bonus 1 finished

--- index.js
+++ index.js
@@ -66,3 +66,59 @@ if(hackerHigherLexOrder === hacker1){
 }else{
     console.log("What?! You both have the same name?")
 }
+
+// Bonus 1
+let longText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi congue convallis nisl, vitae laoreet mauris. Fusce tempus, ipsum nec molestie egestas, magna nisl euismod justo, nec rutrum justo nibh id augue. In id leo sit amet turpis tincidunt commodo ut vitae ante. Cras vulputate elit et molestie auctor. Etiam rhoncus lobortis quam eget elementum. Nullam in enim leo. Ut porttitor sapien vitae rutrum accumsan.
+
+Mauris ullamcorper lacus orci. Suspendisse vehicula blandit enim, non pulvinar enim aliquam id. Nulla vel nisl hendrerit, sagittis leo quis, egestas nunc. Integer fermentum venenatis mattis. Maecenas fermentum neque at nunc vestibulum, at fringilla metus cursus. Vivamus convallis lorem nec ante venenatis porttitor. Donec euismod leo ut leo tempor, eu tristique enim pellentesque. Nullam quis nibh est. Nunc non varius nunc. Vestibulum condimentum vitae purus ut iaculis.
+
+Vestibulum ornare ullamcorper felis, ut egestas velit. Mauris feugiat interdum eros sit amet cursus. Donec in libero non velit faucibus euismod. Ut eu velit vel lectus facilisis dictum nec id dolor. Phasellus varius auctor augue porta mollis. Integer eget interdum nisl. Vestibulum id mollis diam. Ut in sapien lacus. Curabitur aliquam eleifend aliquet. Aenean sodales risus elit, vel convallis eros aliquet a. Fusce sed malesuada velit. Sed id elit nec lectus tincidunt elementum. Suspendisse tempor, mauris sit amet maximus efficitur, ipsum urna commodo nunc, nec faucibus nulla dolor sollicitudin turpis. Maecenas aliquam dapibus nunc, nec rhoncus elit. Pellentesque libero libero, aliquam sed est sit amet, efficitur fringilla urna.`
+// longText = "Hola Mundo"
+
+let wordCounter = 0
+let word = ""
+let etCounter = 0
+for(let i = 0; i < longText.length; i++){
+    if (longText[i] !== " " && longText[i] !== "\n"){
+        word += longText[i]
+    }else{
+        if(word.length > 0){
+            wordCounter++;
+        }
+        // console.log(`${wordCounter}) ${word}`)
+        if(word === "et"){
+            etCounter++;
+        }
+        word = ""; 
+    }
+}
+// If word is not empty, but neither " ", nor /n was not found after it, then it is the last word, and it has not been counted yet. So add it to the count.
+if (word.length > 0) {
+    wordCounter++;
+}
+console.log(`There are ${wordCounter} words in the paragraphs`)
+console.log(`There are ${etCounter} "et" words in the paragraphs`)
+// console.log(`There are ${lineBreakCounter} line breaks`)
+
+// let startIndex = 0 
+// let lastIndexWord = 0
+// for(let i = 0; i < longText.length; i++){    
+//     if (longText[i] == " "  || longText[i] == "\n"){
+//         lastIndexWord = i;
+//         break;
+//     }   
+// }
+// palabra = longText.slice(startIndex, lastIndexWord)
+// console.log(`palabra: ${palabra}`)
+
+// let wordsArray =[]
+// for(let i = 0; i < longText.length; i++){
+//     if (longText[i] !== " " && longText[i] !== "\n"){
+//         // console.log(`i:${i}) char:${longText[i]}`)
+//         word += longText[i]
+//     }else{
+//         wordsArray.push(word)
+//         word = ""; 
+//     }
+// }
+// console.log(wordsArray)
